e230c8cd6975318891184c9bfb05115686632905
Aclmin01 <Aclmin01@example.com>
2026-07-03 02:39:26 +0700

    add fix-node script

---
+++ fix-node.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.zshrc
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+echo "Dang xoa cac file node loi (Ban se can nhap mat khau may Mac, go roi Enter, khong hien dau *)..."
+sudo rm -rf /usr/local/bin/node
+sudo rm -rf /usr/local/bin/npm
+
+echo "Dang cai dat Node 18..."
+nvm install 18
+nvm use 18
+nvm alias default 18
+
+echo "Kiem tra phien ban:"
+node -v
+
+echo "CAI DAT XONG! Ban hay TAT HANG cua so Terminal nay, mo Terminal MOI roi tiep tuc nhe!"
